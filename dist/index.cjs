22a2518dd8d7be3109c2d541387a985fa11a0d8e
whiteSHADOW1234 <enkaihuang2021@gmail.com>
2025-04-13 16:44:48 +0800

    refactor: update module imports to use CommonJS syntax and adjust build script for compatibility

--- dist/index.cjs
+++ dist/index.cjs
@@ -1,73 +1,12 @@
-import { createRequire as __WEBPACK_EXTERNAL_createRequire } from "module";
-/******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
-
-/***/ 473:
-/***/ ((module) => {
-
-module.exports = eval("require")("axios");
-
-
-/***/ }),
-
-/***/ 740:
-/***/ ((module) => {
-
-module.exports = eval("require")("jsdom");
-
-
-/***/ }),
-
-/***/ 896:
-/***/ ((module) => {
-
-"use strict";
-module.exports = __WEBPACK_EXTERNAL_createRequire(import.meta.url)("fs");
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __nccwpck_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		var threw = true;
-/******/ 		try {
-/******/ 			__webpack_modules__[moduleId](module, module.exports, __nccwpck_require__);
-/******/ 			threw = false;
-/******/ 		} finally {
-/******/ 			if(threw) delete __webpack_module_cache__[moduleId];
-/******/ 		}
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/compat */
-/******/ 	
-/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
-/******/ 	
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = new URL('.', import.meta.url).pathname.slice(import.meta.url.match(/^file:\/\/\/\w:/) ? 1 : 0, -1) + "/";
+/******/ 
 /************************************************************************/
 var __webpack_exports__ = {};
-const fs = __nccwpck_require__(896);
-const axios = __nccwpck_require__(473);
-const { JSDOM } = __nccwpck_require__(740);
+const fs = require('fs');
+const axios = require('axios');
+const { JSDOM } = require('jsdom');
 
 const layout = core.getInput('layout', { required: true });
 
@@ -116,6 +55,3 @@ fs.writeFileSync('merged.svg', dom.serialize());
 }
 
 main();
-module.exports = __webpack_exports__;
-/******/ })()
-;
